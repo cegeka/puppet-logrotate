313801cdb2f547edf7dbc1799637f3d6de1248da
fdammeke <fabian.dammekens@cegeka.be>
2013-12-16 11:09:35 +0100

    Small example

--- examples/init.pp
+++ examples/init.pp
@@ -1,1 +1,15 @@
-include logrotate
+logrotate::rule { 'messages':
+  path         => '/var/log/messages',
+  rotate       => 5,
+  rotate_every => 'week',
+  postrotate   => '/usr/bin/killall -HUP syslogd',
+}
+
+logrotate::rule { 'apache':
+  path          => '/var/log/httpd/*.log',
+  rotate        => 5,
+  mail          => 'test@example.com',
+  size          => '100k',
+  sharedscripts => true,
+  postrotate    => '/etc/init.d/httpd restart',
+}
